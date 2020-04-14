37b593f59fbe145522e50fbb9c7251839fe28bf2
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-04-14 11:39:56 +0700

    Tugas 6 PHP

---
+++ tugas6_2.php
@@ -0,0 +1,14 @@
+<?php
+$a = 15000;
+$b = 6;
+$c = 5;
+$d = 2000;
+
+$hsl = $a * ($c * $b) + 2 * $d;
+echo "============================================== <br>";
+echo "Harga Jambu = Rp $a / Kg <br>";
+echo "Harga Kardus = Rp $d / pcs <br>";
+echo "Total Penjualan (Dus dan Jambu = Rp $hsl <br>";
+echo "==============================================";
+
+?>
